a5a7966c651e5c29fb3be83385788fa5463d827a
Brianabeno <brianabeno@gmail.com>
2020-09-04 21:43:08 -0500

    Starter images

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as Footer } from './Footer/Footer';
-export { default as InfoSection } from './InfoSection/InfoSection';
-export { default as Pricing } from './Pricing/Pricing';
